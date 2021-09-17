251a2661247a9d7d2a9b87d40b708c6b65641324
Drew Keat <thedrewkeat@gmail.com>
2021-09-17 13:39:40 -0600

    started

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('%c HI', 'color: firebrick')
+const IMAGES = [];
+
+document.addEventListener("DOMContentLoaded", function () {
+  getImages();
+  placeImages();
+});
+
+function getImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+  fetch(imgUrl)
+  .then((response) => response.json())
+  .then((json) => Object.assign(IMAGES, json.message));
+}
+
+function placeImages() {
+  IMAGES.forEach((data) => console.log(data));
+}
